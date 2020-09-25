e2f659521ccb9e95781bb500a52e44eda7504d5b
edwar vilchez <edwarvilchez1977@gmail.com>
2020-09-25 00:33:51 -0400

    primer commit con la estructura del proyecto, se crea el módulo admin y sus estilos

---
+++ admin/inc/header.php
@@ -0,0 +1,15 @@
+<div id='header'>
+	<div id='logo'>
+		<h2>
+			<a href="index.php">e-Learning Wao</a>
+		</h2>
+	</div>
+	<div id="title">
+		<h2>Admin Panel of e-Learning System</h2>
+	</div>
+	<div id="link">
+		<h3>
+			<a href="#">Logout</a>
+		</h3>
+	</div>
+</div>
---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>e-Learning Wao Admin | Home</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+	<!--include the file header-->
+	<?php include("inc/header.php"); ?>
+
+
+	<h2>Test</h2>
+
+
+	
+</body>
+</html>
